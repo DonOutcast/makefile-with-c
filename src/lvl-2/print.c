586b7febb53d8ca01bba1e0b6e659767fac06dbd
DonOutcast <sham1996@yandex.ru>
2022-05-30 23:52:33 +0300

    lvl-2

---
+++ src/lvl-2/print.c
@@ -0,0 +1,10 @@
+#include "print.h"
+
+int main(void) {
+    print_make();
+    return 0;
+}
+
+void print_make() {
+    printf("This is a Makefile lvl - 2 \n");
+}
---
+++ src/lvl-2/print.h
@@ -0,0 +1,5 @@
+#ifndef SRC_LVL2_PRINT_H_
+#define SRC_LVL2_PRINT_H_
+#include <stdio.h>
+void print_make();
+#endif //  SRC_LVL2_PRINT_H
