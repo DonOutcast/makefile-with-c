e499e3e3d147d84c07c4b869c776642b1f0a368e
DonOutcast <sham1996@yandex.ru>
2022-05-31 11:53:50 +0300

    lvl-3

---
+++ src/lvl-3/print.c
@@ -0,0 +1,9 @@
+#include "print.h"
+
+int main(void) {
+    print_make();
+    return 0;
+}
+void print_make() {
+    puts("This is a Makefile lvl - 3");
+}
---
+++ src/lvl-3/print.h
@@ -0,0 +1,5 @@
+#ifndef SRC_LVL3_PRINT_H_
+#define SRC_LVL3_PRINT_H_
+#include <stdio.h>
+void print_make();
+#endif  //  SRC_LVL3_PRINT_H_
